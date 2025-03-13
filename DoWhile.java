a1263308831f9a0e8b4531312f126294c2c43c49
hgolfetto <golfettohenrique@gmail.com>
2025-03-13 20:03:42 -0300

    style - Example Do while

---
+++ DoWhile.java
@@ -0,0 +1,13 @@
+public class DoWhile {
+    public static void main(String[] args) {
+        int n = 8, c = 1, m = n;
+
+        System.out.println("Os multiplos de " + n + "são: ");
+
+        do{ // pelo menos uma vez será executada
+            System.out.println(m);
+            c++;
+            m = n * c;
+        } while (m < 100); //Se a condição for falsa, acaba o programa
+    }
+}
